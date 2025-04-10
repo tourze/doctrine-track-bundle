c7b465bb507a8c18d1db466097a3c3dc58367dc4
tourze-ci <you@example.com>
2025-04-10 05:30:44 +0000

    feat(doctrine-track-bundle): 新增实体变更跟踪功能

---
+++ src/Entity/EntityTrackLog.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Tourze\DoctrineTrackBundle\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
+use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
+use Tourze\DoctrineUserBundle\Attribute\CreatedByColumn;
+use Tourze\ScheduleEntityCleanBundle\Attribute\AsScheduleClean;
+
+#[AsScheduleClean(expression: '22 5 * * *', defaultKeepDay: 180, keepDayEnv: 'ENTITY_TRACK_LOG_PERSIST_DAY_NUM')]
+#[ORM\Entity]
+#[ORM\Table(name: 'entity_track_log', options: ['comment' => '数据变更日志'])]
+class EntityTrackLog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER, options: ['comment' => 'ID'])]
+    private ?int $id = 0;
+
+    #[IndexColumn]
+    #[ORM\Column(length: 255)]
+    private ?string $objectClass = null;
+
+    #[IndexColumn]
+    #[ORM\Column(length: 40)]
+    private ?string $objectId = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $action = null;
+
+    #[ORM\Column]
+    private array $data = [];
+
+    #[CreatedByColumn]
+    #[ORM\Column(nullable: true, options: ['comment' => '创建人'])]
+    private ?string $createdBy = null;
+
+    #[ORM\Column(length: 45, nullable: true, options: ['comment' => '创建时IP'])]
+    private ?string $createdFromIp = null;
+
+    #[ORM\Column(length: 64, nullable: true, options: ['comment' => '请求ID'])]
+    private ?string $requestId = null;
+
+    #[IndexColumn]
+    #[CreateTimeColumn]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
+    private ?\DateTimeInterface $createTime = null;
+
+    public function setCreateTime(?\DateTimeInterface $createdAt): self
+    {
+        $this->createTime = $createdAt;
+
+        return $this;
+    }
+
+    public function getCreateTime(): ?\DateTimeInterface
+    {
+        return $this->createTime;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getObjectClass(): ?string
+    {
+        return $this->objectClass;
+    }
+
+    public function setObjectClass(string $objectClass): static
+    {
+        $this->objectClass = $objectClass;
+
+        return $this;
+    }
+
+    public function getObjectId(): ?string
+    {
+        return $this->objectId;
+    }
+
+    public function setObjectId(string $objectId): static
+    {
+        $this->objectId = $objectId;
+
+        return $this;
+    }
+
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    public function setAction(string $action): static
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function setCreatedBy(?string $createdBy): static
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?string
+    {
+        return $this->createdBy;
+    }
+
+    public function getCreatedFromIp(): ?string
+    {
+        return $this->createdFromIp;
+    }
+
+    public function setCreatedFromIp(?string $createdFromIp): static
+    {
+        $this->createdFromIp = $createdFromIp;
+
+        return $this;
+    }
+
+    public function getRequestId(): ?string
+    {
+        return $this->requestId;
+    }
+
+    public function setRequestId(?string $requestId): static
+    {
+        $this->requestId = $requestId;
+
+        return $this;
+    }
+}
---
+++ src/EventSubscriber/EntityTrackListener.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Tourze\DoctrineTrackBundle\EventSubscriber;
+
+use Carbon\Carbon;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\Common\Util\ClassUtils;
+use Doctrine\ORM\Event\PostPersistEventArgs;
+use Doctrine\ORM\Event\PostRemoveEventArgs;
+use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Event\PreRemoveEventArgs;
+use Doctrine\ORM\Events;
+use Psr\Log\LoggerInterface;
+use Psr\SimpleCache\CacheInterface;
+use RequestIdBundle\Service\RequestIdStorage;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Contracts\Service\ResetInterface;
+use Tourze\DoctrineAsyncBundle\Service\DoctrineService;
+use Tourze\DoctrineHelper\CacheHelper;
+use Tourze\DoctrineHelper\ReflectionHelper;
+use Tourze\DoctrineTrackBundle\Attribute\TrackColumn;
+use Tourze\DoctrineTrackBundle\Entity\EntityTrackLog;
+
+/**
+ * 保存特定对象的变更日志，方便我们对其进行审查
+ */
+#[AsDoctrineListener(event: Events::preRemove)]
+#[AsDoctrineListener(event: Events::postRemove)]
+#[AsDoctrineListener(event: Events::postPersist)]
+#[AsDoctrineListener(event: Events::postUpdate)]
+#[AutoconfigureTag('as-coroutine')]
+class EntityTrackListener implements ResetInterface
+{
+    /**
+     * @var array 暂存 entity => id 的关系，后面可能使用到
+     */
+    private array $idMap = [];
+
+    public function __construct(
+        private readonly DoctrineService $doctrineService,
+        private readonly RequestStack $requestStack,
+        private readonly PropertyAccessor $propertyAccessor,
+        private readonly LoggerInterface $logger,
+        private readonly Security $security,
+        private readonly CacheInterface $cache,
+        private readonly RequestIdStorage $requestIdStorage,
+    ) {
+    }
+
+    public function reset(): void
+    {
+        $this->idMap = [];
+    }
+
+    /**
+     * 删除记录前，我们做个标记
+     */
+    public function preRemove(PreRemoveEventArgs $eventArgs): void
+    {
+        $this->idMap[spl_object_hash($eventArgs->getObject())] = $this->propertyAccessor->getValue($eventArgs->getObject(), 'id');
+    }
+
+    /**
+     * 删除记录后，记录到数据库
+     */
+    public function postRemove(PostRemoveEventArgs $eventArgs): void
+    {
+        $changedValues = $this->getChangedValues($eventArgs->getObject());
+        if (empty($changedValues)) {
+            return;
+        }
+        $this->saveLog($eventArgs->getObject(), $changedValues, 'remove');
+    }
+
+    /**
+     * 记录创建日志日志
+     */
+    public function postPersist(PostPersistEventArgs $eventArgs): void
+    {
+        $changedValues = $this->getChangedValues($eventArgs->getObject());
+        if (empty($changedValues)) {
+            return;
+        }
+        $this->saveLog($eventArgs->getObject(), $changedValues, 'create');
+    }
+
+    /**
+     * 更新日志
+     */
+    public function postUpdate(PostUpdateEventArgs $eventArgs): void
+    {
+        $changedValues = $this->getChangedValues($eventArgs->getObject());
+        if (empty($changedValues)) {
+            return;
+        }
+        $this->saveLog($eventArgs->getObject(), $changedValues, 'update');
+    }
+
+    private function getChangedValues(object $entity): array
+    {
+        $changedValues = [];
+
+        foreach (ReflectionHelper::getClassReflection($entity)->getProperties(\ReflectionProperty::IS_PRIVATE) as $property) {
+            $trackColumn = $property->getAttributes(TrackColumn::class);
+            if (empty($trackColumn)) {
+                continue;
+            }
+            $trackColumn = $trackColumn[0]->newInstance();
+            /* @var TrackColumn $trackColumn */
+
+            try {
+                $changedValues[$property->getName()] = $this->propertyAccessor->getValue($entity, $property->getName());
+            } catch (\Throwable $exception) {
+                $this->logger->error('读取参数时发生错误', [
+                    'property' => $property,
+                    'entity' => $entity,
+                    'trackColumn' => $trackColumn,
+                    'exception' => $exception,
+                ]);
+            }
+        }
+
+        return $changedValues;
+    }
+
+    /**
+     * 保存日志
+     */
+    private function saveLog(object $entity, array $changedValues, string $action): void
+    {
+        $id = $this->propertyAccessor->getValue($entity, 'id');
+        if (!$id) {
+            $id = $this->idMap[spl_object_hash($entity)] ?? null;
+        }
+        if (!$id) {
+            $this->logger->error('记录TrackLog时发生未知错误', [
+                'entity' => $entity,
+                'values' => $changedValues,
+                'action' => $action,
+            ]);
+
+            return;
+        }
+
+        $objectClass = ClassUtils::getClass($entity);
+
+        // 查找这个对象，上一次的变更日志，如果一样的话我们就不重复插入记录了
+        $checkKey = implode('_', [
+            $action,
+            CacheHelper::getClassTags($objectClass),
+            $id,
+        ]);
+        $checkHash = md5($checkKey . serialize($changedValues));
+        if ($this->cache->get($checkKey) === $checkHash) {
+            return;
+        }
+
+        $log = new EntityTrackLog();
+        $log->setObjectClass($objectClass);
+        $log->setObjectId($id);
+        $log->setAction($action);
+        $log->setData($changedValues);
+        $log->setCreateTime(Carbon::now());
+        $log->setCreatedBy($this->security->getUser()?->getUserIdentifier());
+        $log->setCreatedFromIp($this->requestStack->getMainRequest() ? $this->requestStack->getMainRequest()->getClientIp() : '');
+        $log->setRequestId(substr($this->requestIdStorage->getRequestId(), 0, 64));
+        $this->doctrineService->asyncInsert($log);
+        // 一天内不会重复处理
+        $this->cache->set($checkKey, $checkHash, 60 * 60 * 24);
+    }
+}
