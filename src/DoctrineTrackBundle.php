0ebe00d6c3cd0fc55d17f008bf3e7614550973e8
github-ci <you@example.com>
2025-06-13 14:53:57 +0800

    新增对 `DoctrineAsyncInsertBundle`、`DoctrineUserBundle` 和 `ScheduleEntityCleanBundle` 的依赖。

--- src/DoctrineTrackBundle.php
+++ src/DoctrineTrackBundle.php
@@ -5,17 +5,23 @@ namespace Tourze\DoctrineTrackBundle;
 use RequestIdBundle\RequestIdBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineAsyncInsertBundle\DoctrineAsyncInsertBundle;
 use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
 use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\DoctrineUserBundle\DoctrineUserBundle;
+use Tourze\ScheduleEntityCleanBundle\ScheduleEntityCleanBundle;
 
 class DoctrineTrackBundle extends Bundle implements BundleDependencyInterface
 {
     public static function getBundleDependencies(): array
     {
         return [
-            RequestIdBundle::class => ['all' => true],
+            DoctrineAsyncInsertBundle::class => ['all' => true],
             DoctrineIndexedBundle::class => ['all' => true],
             DoctrineTimestampBundle::class => ['all' => true],
+            DoctrineUserBundle::class => ['all' => true],
+            RequestIdBundle::class => ['all' => true],
+            ScheduleEntityCleanBundle::class => ['all' => true],
         ];
     }
 }
